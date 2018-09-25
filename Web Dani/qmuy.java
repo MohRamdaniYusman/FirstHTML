eaaa4c0df67e2d7d0af7fc132e97747147351457
Moh Ramdani Yusman <34064216+MohRamdaniYusman@users.noreply.github.com>
2018-09-25 09:24:36 +0700

    Add files via upload

---
+++ Web Dani/qmuy.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class qmuy{
+	public static void main(String[] args){
+	Scanner sc= new Scanner(System.in);
+	int a, b, hasil;
+	System.out.print("Masukan Bilangan pertama : ");
+	a=sc.nextInt();
+	System.out.print("Masukan bilangan kedua :  ");
+	b=sc.nextInt();
+	hasil=a+b;
+	System.out.println("Hasilnya Adalah "+hasil);
+	}
+}
